49c601f9a2e77d281952df9496f065c37dc99ac0
David Fernandes <david.fernandes@rarolabs.com.br>
2020-04-16 20:50:57 -0300

    desafio concluido

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,19 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const newTransaction = new Transaction({ title, value, type });
+
+    const balance = this.transactionsRepository.getBalance();
+
+    if (
+      newTransaction.type === 'outcome' &&
+      balance.total < newTransaction.value
+    ) {
+      throw Error('unauthorized transaction due to lack of balance');
+    }
+
+    return this.transactionsRepository.create(newTransaction);
   }
 }
 
